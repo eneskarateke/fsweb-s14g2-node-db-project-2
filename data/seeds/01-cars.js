a2e27a7aba5037c27d930924501bc561d2665336
eneskarateke <eneskarateke@gmail.com>
2023-06-20 18:39:36 +0300

    seed updated

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,3 +1,35 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+const defaultCars = [
+  {
+    vin: "123",
+    make: "Citroen",
+    model: "C4",
+    mileage: 1234,
+  },
+  {
+    vin: "1234",
+    make: "Toyota",
+    model: "Corolla",
+    mileage: 124334,
+  },
+  {
+    vin: "1235",
+    make: "Peugeout",
+    model: "202",
+    mileage: 134234,
+  },
+  {
+    vin: "1236",
+    make: "Audi",
+    model: "A3",
+    mileage: 12345,
+  },
+];
+
 exports.seed = async function (knex) {
   await knex("cars").truncate();
   await knex("cars").insert(defaultCars);
